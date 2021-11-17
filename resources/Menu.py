345dd02759e2c683af080ce8f995d2d54877dcc8
Renee Wu <57248793+RaytonKu@users.noreply.github.com>
2021-11-17 13:00:58 +0800

    Update Menu.py

--- resources/Menu.py
+++ resources/Menu.py
@@ -22,33 +22,32 @@ class MenuResource(Resource):
             menus = MenuModel.query.all()
             menus = menus_schema.dump(menus).data
             redis_cache.__setitem__(MENU_LIST,menus)
-
         return {"status":"success", "data":menus}, 200
 
     def post(self):
         json_data = request.get_json(force=True)
         if not json_data:
                return {'message': 'No input data provided'}, 400
+            
         # Validate and deserialize input
         data, errors = menu_schema.load(json_data)
         if errors:
             return {"status": "error", "data": errors}, 422
+        
         restaurant_id = RestaurantModel.query.filter_by(id=data['restaurant_id']).first()
         if not restaurant_id:
             return {'status': 'error', 'message': 'Menu Restaurant not found'}, 400
+        
         menu = MenuModel(
             restaurant_id=json_data['restaurant_id'],
             name=json_data['name']
             )
         db.session.add(menu)
         db.session.commit()
-
         result = menu_schema.dump(menu).data
-
         return {'status': "success", 'data': result}, 201
 
 class MenuItemResource(Resource):
-
     def get(self, id):
         menu = MenuModel.query.filter_by(id=id)
         memu = menus_schema.dump(menu).data
