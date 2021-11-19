f886a0667369cc5c4bdbf7850073aa658410a279
Renee Wu <57248793+RaytonKu@users.noreply.github.com>
2021-11-19 15:09:32 +0800

    Update restaurant_service.py

--- restaurant/restaurant_service.py
+++ restaurant/restaurant_service.py
@@ -21,7 +21,7 @@ metrics.info('app_info', 'Application info', version='1.0.3')
 @app.route('/restaurants', methods=['GET'])
 def get_restaurant():
     if len(db.hget('restaurants'))==0:
-        return {"error": "Empty list"},204        
+        return {"error": "The database has no restaurants."},204        
     else:
         datas=db.hkeys("restaurants")
         data= {"restaurants":[]}
@@ -35,14 +35,14 @@ def get_restaurant_by_id(restaurant_id):
     if db.hexists("restaurants", restaurant_id):
         return json.loads(db.hget("restaurants",restaurant_id)), 200
     else:
-        return {"Error": "restaurant not found"},404
+        return {"Error": "No such restaurant exists."},404
 
 @app.route('/restaurant/<restaurant_id>/holiday-hours', methods=['GET'])
 def get_holiday_hours(restaurant_id):
     if db.hexists("holidayHours", restaurant_id):
         return json.loads(db.hget("holidayHours",restaurant_id)), 200
     else:
-        return {"Error": "restaurant not found"},404
+        return {"Error": "No such restaurant exists."},404
 
 @app.route('/restaurant/<restaurant_id>/setHoliday-hours', methods=['POST', 'GET'])
 def set_holiday_hours(store_id):
@@ -53,4 +53,4 @@ def set_holiday_hours(store_id):
         db.hset("holidayHours",restaurant_id, json.dumps(jsonInputString))
         return {"Success": "Holiday-hours updated"}, 200
     else:
-        return {"Error": "restaurant not found"},404
+        return {"Error": "No such restaurant exists."},404
